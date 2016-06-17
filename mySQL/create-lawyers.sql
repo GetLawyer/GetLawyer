1b502747c2c49667f3f9b703b331ba7b645d7c6e
ayoungblood <akirayoungblood@me.com>
2016-06-17 11:48:03 -0600

    Removing extraneous columns

--- mySQL/create-lawyers.sql
+++ mySQL/create-lawyers.sql
@@ -1,11 +1,11 @@
 # create-lawyers.sql
 # Adding example data to the lawyers table
-INSERT INTO lawyers (id,name,organization,address,telephone,email,specialties,photoid,bio,city,state,license,password) 
-VALUES ('8041','Johnny Ringo','Wild West Firm','5625 N Wilmot Rd Tucson, AZ 85750','1-800-514-9784','wwlawyers.net','Personal Injury','','','Tucson','AZ','Degree from the University of Life','ringo1');
-INSERT INTO lawyers (id,name,organization,address,telephone,email,specialties,photoid,bio,city,state,license,password) 
-VALUES ('8604','Ringo Starr','Silver Beetle Lawyers','1 Apple Ln, New York, NY 10001','1-800-814-5199','LaywerHelp.net','Debt,','','','New York','NY','Diploma from the School of Hard Knocks','password');
-INSERT INTO lawyers (id,name,organization,address,telephone,email,specialties,photoid,bio,city,state,license,password) 
-VALUES ('8741','Al Capone','Legit-Lawyers Inc.','718 Court St Pekin, IL 61554','1-800-844-8549','gangsterlawyer.com','Debt, Real Estate','','','Perkin','IL','Degree from The College for Organized Crime','12345');
-INSERT INTO lawyers (id,name,organization,address,telephone,email,specialties,photoid,bio,city,state,license,password) 
-VALUES ('8001','Bonnie Parker','B&C Lawyers','1330 Nadene Dr Marysville, CA 95901','1-800-948-7448','dontgetsued.com','Debt, DUI, Bankruptcy','','','Marysville','CA','Degree from The University of Finance','Clyde4ever');
+INSERT INTO lawyers (id,name,organization,address,telephone,email,area,bio,city,state,password) 
+VALUES ('8041','Johnny Ringo','Wild West Firm','5625 N Wilmot Rd Tucson, AZ 85750','1-800-514-9784','wwlawyers.net','Personal Injury',,'','Tucson','AZ','ringo1');
+INSERT INTO lawyers (id,name,organization,address,telephone,email,area,bio,city,state,password) 
+VALUES ('8604','Ringo Starr','Silver Beetle Lawyers','1 Apple Ln, New York, NY 10001','1-800-814-5199','LaywerHelp.net','Debt,','','New York','NY','password');
+INSERT INTO lawyers (id,name,organization,address,telephone,email,area,bio,city,state,password) 
+VALUES ('8741','Al Capone','Legit-Lawyers Inc.','718 Court St Pekin, IL 61554','1-800-844-8549','gangsterlawyer.com','Debt, Real Estate','','Perkin','IL','12345');
+INSERT INTO lawyers (id,name,organization,address,telephone,email,area,bio,city,state,password) 
+VALUES ('8001','Bonnie Parker','B&C Lawyers','1330 Nadene Dr Marysville, CA 95901','1-800-948-7448','dontgetsued.com','Debt, DUI, Bankruptcy','','Marysville','CA','Clyde4ever');
 
