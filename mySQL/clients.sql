422f71491debdfca4719eca373a4664356c17b76
lordoftime4 <kevin.raschke.kr@gmail.com>
2016-06-17 11:11:12 -0600

    adding clients database file

---
+++ mySQL/clients.sql
@@ -0,0 +1,6 @@
+INSERT INTO clients (id,name,email,password,city,statecode) VALUES ('5194','Skinner, Walter','wskinner@fbi.gov','foxiscrazy','Los Angeles','CA');
+INSERT INTO clients (id,name,email,password,city,statecode) VALUES ('5018','Scully, Dana','dscully@fbi.gov','sensiblepasswd','New York','NY');
+INSERT INTO clients (id,name,email,password,city,statecode) VALUES ('5169','Mulder, Spooky','smulder@fbi.gov','2spooky','Aurora','IL');
+INSERT INTO clients (id,name,email,password,city,statecode) VALUES ('5148','Mulder, Fox','fmulder@fbi.gov','notspooky','New York','NY');
+INSERT INTO clients (id,name,email,password,city,statecode) VALUES ('5111','Mulder, Samantha','sammulder@alienmail.com','amireal43','Salt Lake City','UT');
+INSERT INTO clients (id,name,email,password,city,statecode) VALUES ('5001','Man, Smoking','smokingman@redacted.gov','smoking2life','Fairbanks','AK');
