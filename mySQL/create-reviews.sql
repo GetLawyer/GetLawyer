1564bbf7bd5fa12a9089908ed8a47c8f560c2ebb
ayoungblood <akirayoungblood@me.com>
2016-06-17 11:49:16 -0600

    s/laywer/lawyer/g

--- mySQL/create-reviews.sql
+++ mySQL/create-reviews.sql
@@ -1,12 +1,12 @@
 # create-reviews.sql
 # Adding example data to the reviews table
-INSERT INTO reviews (id,laywerID,clientID,anonymous,rating,title,content) 
+INSERT INTO reviews (id,lawyerID,clientID,anonymous,rating,title,content) 
 VALUES ('1005','8041','5194',0,4,'Thank you','Got me decent compensation for gettin\' shot on the job.');
-INSERT INTO reviews (id,laywerID,clientID,anonymous,rating,title,content) 
+INSERT INTO reviews (id,lawyerID,clientID,anonymous,rating,title,content) 
 VALUES ('1068','8604','5001',0,4.5,'Thanks','Got me out of debt, But I couldnt figure out if he was the walrus or not');
-INSERT INTO reviews (id,laywerID,clientID,anonymous,rating,title,content) 
+INSERT INTO reviews (id,lawyerID,clientID,anonymous,rating,title,content) 
 VALUES ('1694','8741','5194',0,4.8,'Satisfied','Thank you for getting helping me get out of debt! Your methods are the best and fastest!');
-INSERT INTO reviews (id,laywerID,clientID,anonymous,rating,title,content) 
+INSERT INTO reviews (id,lawyerID,clientID,anonymous,rating,title,content) 
 VALUES ('1154','8001','5111',0,1,'Thanks a lot','Got me arrested for attempted robbery during our consultory meeting');
-INSERT INTO reviews (id,laywerID,clientID,anonymous,rating,title,content) 
+INSERT INTO reviews (id,lawyerID,clientID,anonymous,rating,title,content) 
 VALUES ('1265','8601','5194',0,5,'Very Helpful','This lawyer was able to help me very well. Thank you!');
